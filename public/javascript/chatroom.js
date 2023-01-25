40e9fe7e1b35c6bfb7ad1e7d6cffa711a636db59
shaoyunjian <shaoyunjian@gmail.com>
2023-01-25 16:24:58 +0800

    Feat: add logout functionality

--- public/javascript/chatroom.js
+++ public/javascript/chatroom.js
@@ -1,1 +1,16 @@
-console.log("OKKKK")
+const logoutBtn = document.querySelector(".logout-btn")
+
+logoutBtn.addEventListener("click", () => {
+
+  fetchLogoutAPI()
+  async function fetchLogoutAPI() {
+    const response = await fetch("/api/user/auth", {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" }
+    })
+    const jsonData = await response.json()
+    if (jsonData.ok) {
+      window.location = "/login"
+    }
+  }
+})
