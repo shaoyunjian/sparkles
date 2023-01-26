20c9a592b4c0a23d1b2aff1d196102f3cd9d7c7e
shaoyunjian <shaoyunjian@gmail.com>
2023-01-26 21:37:52 +0800

    Chore: add line breaks

--- models/user-schema.js
+++ models/user-schema.js
@@ -1,11 +1,23 @@
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  avatar_url: { type: String, default: "https://d1gnt5bnf8w529.cloudfront.net/default-avatar.jpg" }
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar_url: {
+    type: String,
+    default: "https://d1gnt5bnf8w529.cloudfront.net/default-avatar.jpg"
+  }
 })
 
 
-module.exports = mongoose.model("user", userSchema)
+module.exports = mongoose.model("User", userSchema)
