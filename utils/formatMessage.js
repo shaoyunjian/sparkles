d35d468720f5d165aa397e5003fb8576e16ede8d
shaoyunjian <shaoyunjian@gmail.com>
2023-02-05 21:30:32 +0800

    Feat: build a 1 to 1 real-time chatroom

--- utils/formatMessage.js
+++ utils/formatMessage.js
@@ -1,10 +1,12 @@
 const moment = require("moment")
 
-function formatMessage(id, name, msg) {
+function formatMessage(id, name, msg, roomId) {
   return {
     userId: id,
     username: name,
     message: msg,
+    roomId: roomId,
+    date: moment().format("YYYY-MM-DD"),
     time: moment().format("hh:mm a")
   }
 }
