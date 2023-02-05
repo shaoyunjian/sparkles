051556bed61a634447520b852207587fc17822cc
shaoyunjian <shaoyunjian@gmail.com>
2023-02-05 21:30:05 +0800

    Feat: build a 1 to 1 real-time chatroom

--- public/javascript/chatroom.js
+++ public/javascript/chatroom.js
@@ -1,5 +1,15 @@
-// --------------- logout ------------------
+// ------- get user's info from JWT -------
+const jwt = document.cookie
+const parts = jwt.split(".");
+const payload = JSON.parse(atob(parts[1]))
+const currentUserId = payload.id
+const currentUsername = payload.name
+const currentUserEmail = payload.email
 
+//--------- get current room id -----------
+const currentRoomId = window.location.href.split("/")[4]
+
+// --------------- logout -----------------
 const logoutBtn = document.querySelector(".logout-btn")
 
 logoutBtn.addEventListener("click", () => {
@@ -18,9 +28,11 @@ logoutBtn.addEventListener("click", () => {
 })
 
 
-// ------------- get chat list -------------
+// ------------- get chat list ------------
 
 const chatListScrollbar = document.querySelector("#chat-list-scrollbar")
+
+fetchChatListAPI(currentUserId)
 
 async function fetchChatListAPI(senderId) {
   const response = await fetch("/api/chatroom", {
@@ -38,61 +50,73 @@ async function fetchChatListAPI(senderId) {
       }
     })
     chatListScrollbar.innerHTML += `
-      <div class="chat-list-message-box" data-id="${data._id}">
-        <div class="avatar">${friendName[0]}</div>
-        <div class="name-message">
-          <div class="name">${friendName}</div>
-          <div class="message">${data.last_message}</div>
+      <a href="/chatroom/${data._id}">
+        <div class="chat-list-message-box" data-id="${data._id}">
+          <div class="avatar">${friendName[0]}</div>
+          <div class="name-message">
+            <div class="name">${friendName}</div>
+            <div class="message">${data.last_message}</div>
+          </div>
+          <div class="message-time">${data.last_message_time}</div>
         </div>
-        <div class="message-time">${data.last_message_time}</div>
-      </div>
+      </a>
     `
   })
 
 }
 
+// ---------- render chatroom -------------
 
-//------------- socket io --------------------
+const chatBoxTopBarAvatar = document.querySelector("#chatbox-top-bar-avatar")
+const chatBoxTopBarName = document.querySelector("#chatbox-top-bar-name")
+
+if (currentRoomId) {
+  fetchChatroomAPI(currentRoomId)
+  displayMessageHistory(currentRoomId)
+} else {
+  document.querySelector("#chatbox").classList.add("display-none")
+}
+
+//------ 1 to 1 real-time chatroom --------
 
 const socket = io()
 const chatScrollBar = document.querySelector(".middle-scollbar")
-const chatMessageBox = document.querySelector("#chat-message-box")
+const chatMessageBox = document.querySelector(".chat-message-box")
 const sendMessageBtn = document.querySelector("#send-message-btn")
 const inputMessage = document.querySelector("#input-message")
 chatScrollBar.scrollTop = chatScrollBar.scrollHeight
 
+// join room 
+socket.emit("joinRoom", { currentUserId, currentUsername, currentRoomId })
 
-// --------- send my message ----------------- 
+// socket on event
+socket.on("message", message => {
+  appendMessage(message)
+  if (message.userId === currentUserId) {
+    storeMessageToDB(message)
+  }
+})
 
-fetchCurrentUserAPI()
-async function fetchCurrentUserAPI() {
-  const response = await fetch("/api/user/auth", {
-    method: "GET",
-    headers: { "Content-Type": "application/json" }
-  })
+// leave room
+socket.on("leaveRoom", (msg) => {
+  console.log(msg)
+})
 
-  const jsonData = await response.json()
-  const currentUsername = jsonData.data.name
-  const currentUserId = jsonData.data.id
-  console.log("目前登入的使用者名：", currentUsername)
-  console.log("目前登入的使用者ID：", currentUserId)
+// ------- emit message to server ----------
 
-  // get current chat list
-  fetchChatListAPI(currentUserId)
-  // click on a chat in the list to join the corresponding chat room
-  clickChatList(currentUserId)
+emitMessageToServer(currentUsername, currentUserId)
 
-  // --------------- handle input Messages ----------------
+function emitMessageToServer(currentUsername, currentUserId) {
   inputMessage.addEventListener("keypress", (event) => {
     const inputMessageValue = inputMessage.value
     const msgData = {
       text: inputMessageValue,
       username: currentUsername,
-      id: currentUserId
+      id: currentUserId,
+      roomId: currentRoomId
     }
     if (event.key === "Enter") {
       if (inputMessageValue) {
-        // emit message to server
         socket.emit("chatMessages", msgData)
       }
     }
@@ -103,137 +127,99 @@ async function fetchCurrentUserAPI() {
     const msgData = {
       text: inputMessageValue,
       username: currentUsername,
-      id: currentUserId
+      id: currentUserId,
+      roomId: currentRoomId
     }
     if (inputMessageValue) {
-      // emit message to server
       socket.emit("chatMessages", msgData)
-
     }
   })
 }
 
-
-
-
-// --------- socket on event ---------------- 
-
-socket.on("getMessage", message => {
-  appendFriendsMessage(message)
-  chatScrollBar.scrollTop = chatScrollBar.scrollHeight
-})
-
-socket.on("sendMessage", message => {
-  appendMyMessage(message)
-
-  // store my message into database
-  storeMessageAPI()
-  async function storeMessageAPI() {
-    const response = await fetch("/api/message", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        senderId: message.userId,
-        messageText: message.message,
-        chatroomId: "63d2147931fb09bc0dd8ff9a", //まだ
-        sentTime: message.time
-      })
+// ---- store my message into database -----
+async function storeMessageToDB(message) {
+  const response = await fetch("/api/message", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      senderId: message.userId,
+      messageText: message.message,
+      chatroomId: message.roomId,
+      sentTime: message.time
     })
+  })
+}
+
+// ------ get chatroom information ---------
+async function fetchChatroomAPI(roomId) {
+  const response = await fetch(`/api/chatroom?chatroomId=${roomId}`)
+
+  const jsonData = await response.json()
+  let friendName
+  jsonData.data[0].participants.forEach((value) => {
+    if (value._id !== currentUserId) {
+      friendName = value.name
+    }
+  })
+  chatBoxTopBarAvatar.innerHTML = `<p>${friendName[0]}</p>`
+  chatBoxTopBarName.textContent = friendName
+}
+
+// ----- display text message history ------
+async function displayMessageHistory(roomId) {
+  const response = await fetch(`/api/message/${roomId}`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" }
+  })
+
+  const jsonData = await response.json()
+
+  if (jsonData.data) {
+    chatMessageBox.textContent = ""
+
+    jsonData.data.forEach((data) => {
+      const historyMessage = {
+        userId: data.sender._id,
+        username: data.sender.name,
+        message: data.message_text,
+        time: data.sent_time
+      }
+
+      appendMessage(historyMessage)
+    })
+  } else {
+    chatMessageBox.innerHTML = "沒有任何訊息"
   }
-})
 
+}
 
-// ----------- display my message ----------- 
-
-function appendMyMessage(msg) {
-  chatMessageBox.innerHTML += `              
+// --------- append message bubble --------
+function appendMessage(msg) {
+  const myMessage = `              
     <div class="sender-info">
       <div class="sender-name">You</div>
       <div class="sending-time">${msg.time}</div>
     </div>
     <div class="sender-bubble bubble">${msg.message}</div>
   `
-  chatScrollBar.scrollTop = chatScrollBar.scrollHeight
-  inputMessage.value = ""
-}
 
-// ---------- display friend's message ----------- 
-
-function appendFriendsMessage(msg) {
-  chatMessageBox.innerHTML += `
+  const friendMessage = `
     <div class="receiver-info">
       <div class="receiver-name">${msg.username}</div>
       <div class="receiving-time">${msg.time}</div>
     </div>
     <div class="receiver-bubble bubble">${msg.message}</div>
   `
-}
 
+  chatMessageBox.innerHTML = ""
 
-// ---------- display history messages ----------
+  if (msg.userId === currentUserId) {
+    chatMessageBox.innerHTML += myMessage
+  } else {
+    chatMessageBox.innerHTML += friendMessage
+  }
 
-function clickChatList(senderId) {
-
-  // chatroom avatar and name
-  const chatBoxTopBarAvatar = document.querySelector("#chatbox-top-bar-avatar")
-  const chatBoxTopBarName = document.querySelector("#chatbox-top-bar-name")
-
-  // chatroom messages
-  chatListScrollbar.addEventListener("click", (event) => {
-
-    const roomId = event.target.dataset.id
-    if (!roomId) return
-
-    displayHistoryMessages(roomId)
-    fetchChatroomAPI(roomId)
-
-    async function displayHistoryMessages(roomId) {
-      const response = await fetch(`/api/message/${roomId}`, {
-        method: "GET",
-        headers: { "Content-Type": "application/json" }
-      })
-
-      const jsonData = await response.json()
-
-      if (jsonData.data) {
-        // if there're already messages in the box, stop rendering history messages.
-        if (chatMessageBox.childElementCount) return
-
-        jsonData.data.forEach((data) => {
-          const historyMessage = {
-            userId: data.sender._id,
-            username: data.sender.name,
-            message: data.message_text,
-            time: data.sent_time
-          }
-
-          if (data.sender._id === senderId) {
-            appendMyMessage(historyMessage)
-          } else {
-            appendFriendsMessage(historyMessage)
-          }
-        })
-      } else {
-        chatMessageBox.innerHTML = "沒有任何訊息"
-      }
-
-    }
-
-    async function fetchChatroomAPI(roomId) {
-      const response = await fetch(`/api/chatroom?chatroomId=${roomId}`)
-
-      const jsonData = await response.json()
-      let friendName
-      jsonData.data[0].participants.forEach((value) => {
-        if (value._id !== senderId) {
-          friendName = value.name
-        }
-      })
-      chatBoxTopBarAvatar.innerHTML = `<p>${friendName[0]}</p>`
-      chatBoxTopBarName.textContent = friendName
-    }
-
-  })
-
+  chatScrollBar.scrollTop = chatScrollBar.scrollHeight
+  inputMessage.value = ""
 }
 
