118f7d17f1710d7da6c86d6281bbf2be55286d28
shaoyunjian <shaoyunjian@gmail.com>
2023-02-23 00:28:54 +0800

    Feat: add the functionality to send an image

--- utils/formatMessage.js
+++ utils/formatMessage.js
@@ -1,11 +1,12 @@
 const moment = require("moment")
 
-function formatMessage(id, name, avatar, msg, roomId) {
+function formatMessage(id, name, avatar, msg, imageUrl, roomId) {
   return {
     userId: id,
     username: name,
     avatarUrl: avatar,
     message: msg,
+    messageImageUrl: imageUrl,
     roomId: roomId,
     date: moment().format("YYYY-MM-DD"),
     time: moment().format("hh:mm a")
