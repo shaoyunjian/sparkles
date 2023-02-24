66cacd228bc8db63c663cd4978659d176b3cd68b
shaoyunjian <shaoyunjian@gmail.com>
2023-02-24 17:09:53 +0800

    Chore: move logout function to member-settings.js

--- public/javascript/chatroom.js
+++ public/javascript/chatroom.js
@@ -27,26 +27,6 @@ myName.forEach((name) => {
   name.textContent = currentUsername
 })
 
-// --------------- logout -----------------
-//(unfinished)
-// const logoutBtn = document.querySelector(".logout-btn")
-
-// logoutBtn.addEventListener("click", () => {
-
-//   fetchLogoutAPI()
-//   async function fetchLogoutAPI() {
-//     const response = await fetch("/api/user/auth", {
-//       method: "DELETE",
-//       headers: { "Content-Type": "application/json" }
-//     })
-//     const jsonData = await response.json()
-//     if (jsonData.ok) {
-//       window.location = "/login"
-//     }
-//   }
-// })
-
-
 // ------------- get chat list ------------
 
 const chatListScrollbar = document.querySelector("#chat-list-scrollbar")
