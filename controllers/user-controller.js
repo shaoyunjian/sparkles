e1074a2337a760a8e2b54cc6d46e2bc43fe38575
shaoyunjian <shaoyunjian@gmail.com>
2023-03-16 22:15:58 +0800

    Fix: use brcrytjs for password

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,6 @@
 const User = require("../models/user-schema")
 const mongoDB = require("../config/mongoose")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 const { userDataValidation } = require("../utils/auth")
 const { profileEditValidation } = require("../utils/profile")
